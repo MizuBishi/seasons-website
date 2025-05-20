6a9232f19adc64f33c7a5097a9e86becdeb727f3
Michelle Andrey <michelle.andrey@gridonic.ch>
2025-05-20 21:12:01 +0200

    Refactor index.html to update asset paths and internal links, ensuring all resources are correctly referenced from the src/assets directory. Add new assets including icons, images, and a web manifest for improved application support.

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,13 @@ export default defineConfig({
     outDir: "dist",
     assetsDir: "assets",
     emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: "index.html",
+        faq: "faq.html",
+        datenschutz: "datenschutz.html",
+        impressum: "impressum.html",
+      },
+    },
   },
 });
