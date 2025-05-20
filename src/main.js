c130643453b218e9a29e96c314c67b164d06f4cf
Michelle Andrey <michelle.andrey@gridonic.ch>
2025-05-20 17:35:50 +0200

    feat(content): new content & pages

--- src/main.js
+++ src/main.js
@@ -22,3 +22,19 @@ import { setupCounter } from "./counter.js";
 // `;
 
 // setupCounter(document.querySelector("#counter"));
+
+// Header background on scroll
+const header = document.querySelector(".header");
+let lastScroll = 0;
+
+window.addEventListener("scroll", () => {
+  const currentScroll = window.pageYOffset;
+
+  if (currentScroll > 50) {
+    header.classList.add("header--scrolled");
+  } else {
+    header.classList.remove("header--scrolled");
+  }
+
+  lastScroll = currentScroll;
+});
