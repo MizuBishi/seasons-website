e0454e1df032aae33106acc08fcfc2f04f106d2b
Michelle Andrey <michelle.andrey@gridonic.ch>
2025-05-22 07:42:45 +0200

    Update project name to 'seasons-website' and add homepage URL in package.json; adjust base path in vite.config.js for deployment.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/seasons-website/",
+  base: "/seasons-website",
   build: {
     outDir: "dist",
     assetsDir: "assets",
